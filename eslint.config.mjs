eec72d8dfa64b62e987a649ffb490e2905e08d3d
dlrodev92 <dlrdev92@gmail.com>
2025-03-05 12:17:05 +0000

    test husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error", // Enforce Prettier rules
+    },
+  }
 ];
 
 export default eslintConfig;
