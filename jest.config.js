2e38ac18674cd4ee96271526b2c38c62cb1a89e7
dlrodev92 <dlrdev92@gmail.com>
2025-03-05 12:29:43 +0000

    test added

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: ["@testing-library/jest-dom"],
+    moduleNameMapper: {
+      "^@/(.*)$": "<rootDir>/src/$1",
+    },
+  };
+  
